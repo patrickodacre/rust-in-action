88076d0e80a0124976473a6e683ba579f22c47eb
pwho <patrick@patrickwho.me>
2021-05-06 17:44:41 -0400

    Add notes on pattern matching

---
+++ src/matching.rs
@@ -0,0 +1,32 @@
+pub fn do_matching()
+{
+    let collection = vec![1, 2, 3, 4, 5];
+    for item in &collection {
+        println!("item {:?}", item);
+
+        match item {
+            1 => {
+                println!("First item!");
+            }
+            2..=4 => {
+                println!("Middle items...");
+            }
+            5 => {
+                println!("Last item.");
+                println!("We're done!");
+            }
+            _ => {
+                println!("We have to handle all cases, so a default is needed");
+            }
+        }
+
+        let result = match item {
+            3 => "Hit!",
+            _ => "Miss :(",
+        };
+
+        if result == "Hit!" {
+            println!("Found it! {}", item);
+        }
+    }
+}
