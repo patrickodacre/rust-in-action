d6841a53e2e68ee23bd5659cd3c75baf85e908fa
pwho <patrick@patrickwho.me>
2021-05-07 17:03:42 -0400

    Add more notes on Generics and Traits

--- src/traits_generics_1.rs
+++ src/traits_generics_1.rs
@@ -1,15 +1,12 @@
 pub fn run()
 {
     // lifetimes, generics and traits::
-    let k = Knight::new("Daniel".to_string());
-    let mut m = Mob::new(10);
-    println!("{:?}", m.hp);
+    let mut k = Knight::new("Daniel".to_string(), 20, 100);
+    let mut m = Mob::new(40);
 
     let attacked = k.attack(&mut m);
 
-    println!("Attacked {:?}", attacked);
-
-    println!("{:?}", m.hp);
+    let attacked = m.attack(&mut k);
 }
 
 // I wasn't able to get this to work as an associated type to the HasHealth trait
@@ -30,13 +27,9 @@ struct Mob
 
 impl Knight
 {
-    fn new(name: String) -> Self
+    fn new(name: String, atk: u32, hp: u32) -> Self
     {
-        Knight {
-            name,
-            atk: 20,
-            hp: 100,
-        }
+        Knight { name, atk, hp }
     }
 }
 
@@ -50,11 +43,15 @@ impl Mob
 
 trait HasHealth
 {
+    type HealthPoints: std::fmt::Display + std::fmt::Debug;
+
     fn decrease_health(&mut self, amount: Hp) -> Result<(), &'static str>;
+    fn health_points(&self) -> Self::HealthPoints;
 }
 
 impl HasHealth for Knight
 {
+    type HealthPoints = u32;
     fn decrease_health(&mut self, amount: Hp) -> Result<(), &'static str>
     {
         // instant death
@@ -72,11 +69,17 @@ impl HasHealth for Knight
         self.hp = new_hp;
 
         Ok(())
+    }
+
+    fn health_points(&self) -> Self::HealthPoints
+    {
+        self.hp
     }
 }
 
 impl HasHealth for Mob
 {
+    type HealthPoints = u32;
     fn decrease_health(&mut self, amount: Hp) -> Result<(), &'static str>
     {
         // instant death
@@ -95,10 +98,21 @@ impl HasHealth for Mob
 
         Ok(())
     }
+
+    fn health_points(&self) -> Self::HealthPoints
+    {
+        self.hp
+    }
 }
 
 trait CanAttack
 {
+    // by bounding this to the trait HasHealth I'm saying that this function
+    // needs to operate on a type the impl that trait.
+    // if my intention is to access Mob / Knight struct properties on attackee
+    // but still use generics, then the new trait will have to expose
+    // getters and setters for those properties. The impl will then
+    // point caller to those properties on the implementing struct
     fn attack<T: HasHealth>(&self, attackee: &mut T) -> Result<(), &'static str>;
 }
 
@@ -106,9 +120,14 @@ impl CanAttack for Knight
 {
     fn attack<T: HasHealth>(&self, mob: &mut T) -> Result<(), &'static str>
     {
+        println!("Knight is attacking!!");
+
         if !mob.decrease_health(self.atk).is_ok() {
             return Err("Attack failed.");
         }
+
+        // I've extended the Display trait
+        println!("Remaining health points {}\n", mob.health_points());
 
         Ok(())
     }
@@ -118,9 +137,14 @@ impl CanAttack for Mob
 {
     fn attack<T: HasHealth>(&self, knight: &mut T) -> Result<(), &'static str>
     {
+        println!("Mob is attacking!!");
+
         if !knight.decrease_health(self.atk).is_ok() {
             return Err("Attack failed.");
         }
+
+        // I've also extended the Debug trait
+        println!("Remaining health points {:?}\n", knight.health_points());
 
         Ok(())
     }
