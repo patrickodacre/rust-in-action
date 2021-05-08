900d8b85707f31e503283fdec469d32b974dc80b
PWho <patrick@patrickwho.me>
2021-05-07 22:36:02 -0400

    Add new state enum and close method to errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -4,7 +4,8 @@ use std::str;
 pub fn run() {
     println!("Running Errors Module");
 
-    let f = File::new("MyFile.txt", "Hello, World!");
+    let mut f = File::new("MyFile.txt", "Hello, World!");
+    println!("File State:: {:?}", f.state);
     println!("F Data:: {:?}", f.data);
 
     let mut buffer = Vec::<u8>::new();
@@ -15,18 +16,35 @@ pub fn run() {
         }
     }
 
+    println!("File State:: {:?}", f.state);
+    match f.close() {
+        Ok(_) => {}
+        Err(err) => {
+            println!("Error :: {:?}", err);
+        }
+    }
+
+    println!("File State:: {:?}", f.state);
     println!("Buffer:: {:?}", buffer);
+}
+
+#[derive(Debug)]
+enum FileState {
+    Open,
+    Closed,
 }
 
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
+    state: FileState,
 }
 
 #[derive(Debug)]
 enum FileError {
     FileNotFound,
+    FailedToClose,
 }
 
 impl File {
@@ -34,10 +52,13 @@ impl File {
         File {
             name: name.to_string(),
             data: data.as_bytes().to_vec(),
+            state: FileState::Closed,
         }
     }
 
-    fn open(&self, buffer: &mut Vec<u8>) -> Result<(), FileError> {
+    fn open(&mut self, buffer: &mut Vec<u8>) -> Result<(), FileError> {
+        self.state = FileState::Open;
+
         // randomly fail to open the "file"
         if random() {
             return Err(FileError::FileNotFound);
@@ -46,6 +67,16 @@ impl File {
         for item in self.data.iter() {
             buffer.push(*item);
         }
+
+        Ok(())
+    }
+
+    fn close(&mut self) -> Result<(), FileError> {
+        if random() {
+            return Err(FileError::FailedToClose);
+        }
+
+        self.state = FileState::Closed;
 
         Ok(())
     }
