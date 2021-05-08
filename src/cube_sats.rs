ff44db0d9a5b8af1c158bde7be4088f0038831cf
pwho <patrick@patrickwho.me>
2021-05-08 18:31:16 -0400

    Update CubeSat to be a struct

--- src/cube_sats.rs
+++ src/cube_sats.rs
@@ -14,23 +14,23 @@ struct CubeSat
 
 pub fn run()
 {
-    let sat_a = 0;
-    let sat_b = 1;
-    let sat_c = 2;
+    let sat_a = CubeSat { id: 0 };
+    let sat_b = CubeSat { id: 1 };
+    let sat_c = CubeSat { id: 2 };
 
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
+    let a_status = check_status(&sat_a);
+    let b_status = check_status(&sat_b);
+    let c_status = check_status(&sat_c);
     println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
     // "waiting" ...
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
+    let a_status = check_status(&sat_a);
+    let b_status = check_status(&sat_b);
+    let c_status = check_status(&sat_c);
     println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 }
 
-fn check_status(sat_id: u64) -> StatusMessage
+fn check_status(cube_sat: &CubeSat) -> StatusMessage
 {
     StatusMessage::Ok
 }
