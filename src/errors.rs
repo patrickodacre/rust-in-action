9068f545bd39ca612b41208be6620c91e6860ec6
pwho <patrick@patrickwho.me>
2021-05-07 20:38:02 -0400

    init errors

---
+++ src/errors.rs
@@ -0,0 +1,18 @@
+pub fn run()
+{
+    println!("Running Errors Module");
+}
+
+struct File
+{
+    name: String,
+    data: Vec<u8>,
+}
+
+impl File
+{
+    fn new() -> Self
+    {
+        File {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+mod errors;
 mod new_type;
 
 fn main()
 {
-    new_type::run();
+    errors::run();
 }
