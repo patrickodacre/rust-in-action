21ccfd805a745d9d0eb18ad23e156077a5685946
PWho <patrick@patrickwho.me>
2021-05-08 22:16:05 -0400

    Refactor to better match the book's example
    - simplify / divide up the code better
    - better use of using Result and Option

--- src/cube_sats.rs
+++ src/cube_sats.rs
@@ -7,31 +7,24 @@
 #![allow(unused_variables)]
 
 #[derive(Debug)]
-enum StatusMessage
-{
-    Ok,
+enum ConnectionError {
+    HostRefusedConnection
 }
 
 #[derive(Debug)]
 struct CubeSat
 {
     id: u64,
-    mailbox: Mailbox,
 }
 
 impl CubeSat
 {
     fn new(id: u64) -> Self
     {
-        CubeSat {
-            id,
-            mailbox: Mailbox {
-                messages: Vec::<Message>::new(),
-            },
-        }
+        CubeSat { id }
     }
 
-    fn recv(&mut self, mailbox: &mut Mailbox) -> Option<Message>
+    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message>
     {
         mailbox.deliver(&self)
     }
@@ -80,42 +73,60 @@ impl GroundStation
 {
     /// send a message to a CubeSat
     /// it's ok for this function to take ownership of the message
-    fn send(mailbox: &mut Mailbox, msg: Message)
+    fn send(&self, mailbox: &mut Mailbox, msg: Message)
     {
         mailbox.post(msg)
     }
+
+    fn connect(&self, id: u64) -> Result<CubeSat, ConnectionError>
+    {
+        if id == 0 {
+            return Err(ConnectionError::HostRefusedConnection)
+        }
+
+        Ok(CubeSat::new(id))
+    }
+}
+
+fn get_sat_ids() -> Vec::<u64>
+{
+    vec![0,1,2]
 }
 
 pub fn run()
 {
-    let mut sat_a = CubeSat::new(0);
-    let sat_b = CubeSat::new(1);
-    let sat_c = CubeSat::new(2);
-
-    let a_status = check_status(&sat_a);
-    let b_status = check_status(&sat_b);
-    let c_status = check_status(&sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
-
-    // "waiting" ...
-    let a_status = check_status(&sat_a);
-    let b_status = check_status(&sat_b);
-    let c_status = check_status(&sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
-
     let mut mailbox = Mailbox::new();
+    let base = GroundStation {};
 
-    let msg = Message {to: sat_a.id, body: "Some Message".to_string()};
+    // we could be getting these ids from a database 
+    let sat_ids = get_sat_ids();
 
-    // I don't need the msg after this, so I can give ownership to the send() function
-    GroundStation::send(&mut mailbox, msg);
+    for item in sat_ids {
 
-    let msg = sat_a.recv(&mut mailbox);
+        // this is mimicing a connection where, presumably, some verification, etc. is done
+        match base.connect(item) {
+            Ok(sat) => {
 
-    println!("msg received {:?}", msg);
-}
+                let msg = Message {to: sat.id, body: format!("Some Message to {}", item)};
 
-fn check_status(cube_sat: &CubeSat) -> StatusMessage
-{
-    StatusMessage::Ok
+                // I don't need the msg after this, so I can give ownership to the send() function
+                base.send(&mut mailbox, msg)
+            }
+            Err(err) => {
+                println!("Could not connect to sat {}", item);
+            }
+        }
+    }
+
+    if let Ok(sat) = base.connect(1) {
+        let msg = sat.recv(&mut mailbox).unwrap();
+
+        println!("msg received {:?}", msg);
+    }
+
+    if let Ok(sat) = base.connect(2) {
+        let msg = sat.recv(&mut mailbox).unwrap();
+
+        println!("msg received {:?}", msg);
+    }
 }
